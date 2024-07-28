dc4c3580afb7512922e7340c34cfbb1ba84b2315
DanyaGTx <raxub007@gmail.com>
2024-07-28 18:38:46 +0200

    changed cors origin

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
-
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use('/api', productRoutes);
 app.use('/api', orderRoutes);
+
+app.use(cors({ origin: "*" }));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
